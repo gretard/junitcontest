c744f63939e5fd44dc324a8b161ba8d3d1d7359e
gretard <gretard@users.noreply.github.com>
2019-03-21 16:16:27 +0200

    Update paths handling

--- src/pit-runner/src/main/java/sbst/pit/runner/metrics/TestMetricsCollector.java
+++ src/pit-runner/src/main/java/sbst/pit/runner/metrics/TestMetricsCollector.java
@@ -2,6 +2,7 @@ package sbst.pit.runner.metrics;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import sbst.pit.runner.App.Modes;
@@ -18,30 +19,29 @@ public class TestMetricsCollector implements IExecutor {
 		String baseDir = request.baseDir;
 		File metricsFile = Paths.get(baseDir, "testMetrics.csv").toFile();
 
-	
 		Utils.deleteOld(Paths.get(baseDir, "testMetrics.csv"), false);
+		
 		Files.walk(Paths.get(baseDir), 9999)
-				.filter(x -> x.toFile().getAbsolutePath().contains("temp" + File.separator + "bin" + File.separator))
-				.forEach(path -> {
-					if (!path.toFile().getAbsolutePath().endsWith(".class")) {
+				.filter(x -> x.toFile().getAbsolutePath().contains("temp" + File.separator + "bin")).forEach(x -> {
+					if (!x.toFile().getAbsolutePath().endsWith(".class")) {
 						return;
 					}
+					Path u = Paths.get(x.toString().split("temp", 2)[0]);
 
-					String filePath = path.toFile().getAbsolutePath().split("results")[1];
-					String p = path.toFile().getAbsolutePath().split("temp.bin")[0];
-					String classPath = Paths.get(p, "temp", "bin").toFile().getAbsolutePath();
+					final String run = u.getFileName().toString();
+					final String benchmark = u.getFileName().toString().split("_", 2)[0];
+					final String tool = u.getParent().toString().split("results_")[1].split("_")[0];
+					final String budget = u.getParent().toString().split("results_")[1].split("_")[1];
+					final String classPath = Paths.get(u.toString(), "temp", "bin").toFile().getAbsolutePath();
+					final String classzRaw = x.toString().split("temp", 2)[1].split("bin", 2)[1];
+					final String classz = classzRaw.substring(1, classzRaw.length() - 6).replace(File.separatorChar,
+							'.');
 
-					String[] temp = filePath.replace(File.separatorChar, '.').split("_", 4);
-					String tool = temp[1];
-					String budget = temp[2].split("\\.")[0];
-					String benchmark = temp[2].split("\\.")[1];
-					String classzName = temp[3].split("temp\\.bin")[1];
-					classzName = classzName.substring(1, classzName.length() - 6);
 					BaseRequest baseRequest = new BaseRequest();
-					baseRequest.additionalInfoHeader = "benchmark\ttool\tbudget\t";
-					baseRequest.additionalInfo = benchmark + "\t" + tool + "\t" + budget + "\t";
+					baseRequest.additionalInfoHeader = "benchmark\ttool\tbudget\trun\t";
+					baseRequest.additionalInfo = benchmark + "\t" + tool + "\t" + budget + "\t" + run + "\t";
 					baseRequest.classpath.add(classPath);
-					baseRequest.classes.add(classzName);
+					baseRequest.classes.add(classz);
 					collector.collectMetrics(baseRequest, metricsFile);
 				});
 
